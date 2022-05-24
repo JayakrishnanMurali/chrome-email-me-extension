faed5c72cb81a213d127fff96c353962ba3695b6
Jayakrishnan <jayakrishnan1012@gmail.com>
2022-05-24 13:44:01 +0530

    Add chrome extension for sending email of current page

---
+++ options.js
@@ -0,0 +1,31 @@
+var gmail = "https://mail.google.com/mail/?extsrc=mailto&url=%s";
+
+function toggle(radioButton) {
+  if (window.localStorage == null) {
+    alert("Local storage is required for changing providers");
+    return;
+  }
+  if (document.getElementById("gmail").checked) {
+    window.localStorage.customMailtoUrl = gmail;
+  } else {
+    window.localStorage.customMailtoUrl = "";
+  }
+}
+
+function main() {
+  if (window.localStorage == null) {
+    alert("LocalStorage must be enabled for changing options.");
+    document.getElementById("default").disabled = true;
+    document.getElementById("gmail").disabled = true;
+    return;
+  }
+
+  if (window.localStorage.customMailtoUrl == gmail)
+    document.getElementById("gmail").checked = true;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  main();
+  document.querySelector("#default").addEventListener("click", toggle);
+  document.querySelector("#gmail").addEventListener("click", toggle);
+});
